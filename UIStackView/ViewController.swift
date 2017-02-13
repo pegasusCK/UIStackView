8257fe8869c4dec4ce9ff8500421c04b8281891e
pegasusCK <cky1026@gmail.com>
2017-02-13 15:43:36 -0800

    First Commit

---
+++ UIStackView/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  UIStackView
+//
+//  Created by Chihkao Yu on 2/13/17.
+//  Copyright © 2017 Chihkao Yu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
